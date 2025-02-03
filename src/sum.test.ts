fe41806003996c048386ed54fa794f76635041ab
sagarkavar <sagarpatel1721999@gmail.com>
2025-02-03 15:43:41 +0530

    write testCases for TDD approach

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,7 +1,38 @@
-import { sum } from './sum';
+import { StringCalculator } from "./stringCalculator";
 
-describe('sum function testcases', () => {
-  it('should return 3 for 1 + 2', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-})
+describe('StringCalculator', () => {
+    let calculator: StringCalculator;
+
+    beforeEach(() => {
+        calculator = new StringCalculator();
+    });
+
+    test('should return 0 for an empty string', () => {
+        expect(calculator.add("")).toBe(0);
+    });
+
+    test('should return the number itself if only one number is provided', () => {
+        expect(calculator.add("1")).toBe(1);
+    });
+
+    test('should return the sum of two numbers', () => {
+        expect(calculator.add("1,2")).toBe(3);
+    });
+
+    test('should return the sum of multiple numbers', () => {
+        expect(calculator.add("1,2,3,4")).toBe(10);
+    });
+
+    test('should handle new lines as delimiters', () => {
+        expect(calculator.add("1\n2,3")).toBe(6);
+    });
+
+    test('should support custom delimiters', () => {
+        expect(calculator.add("//;\n1;2")).toBe(3);
+        expect(calculator.add("//|\n1|2|3")).toBe(6);
+    });
+
+    test('should throw an error for negative numbers', () => {
+        expect(() => calculator.add("1,-2,3,-4")).toThrow("Negative numbers not allowed: -2, -4");
+    });
+});
